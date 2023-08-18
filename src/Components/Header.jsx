32914f44da8f26457f4497db1df27241d8fe5121
ayashadevikar <ayashadevikar8@gmail.com>
2023-08-18 19:39:26 +0530

    some changes added

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+
 const Header = () => {
   return (
    <nav className='navbar'>
@@ -10,4 +11,4 @@ const Header = () => {
   )
 }
 
-export default Header
+export default Header;
